d45bc54dfe2c5962e84c0012df37a7b3fcdcf695
OfficialMarvin <mkj5388@psu.edu>
2023-03-29 14:55:20 +0200

    make svg boats

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ for (let i = 0; i < gridSize; i++) {
   grid[i] = new Array(gridSize).fill(0);
 }
 
+
 // Allow the user to place 1x1 ships on the grid
 const placeShip = (x, y) => {
   if (grid[x][y] === 0) {
@@ -21,7 +22,42 @@ for (let i = 1; i < 4; i++) {
     let y = prompt('Enter the y coordinate for your ship number ' + i);
     placeShip(x, y);
 }
+const cellSize = 100;
+const svg = document.getElementById('gridcontainter');
 
+
+function drawGrid() {   
+    for (let y = 0; y < grid.length; y++) {   
+        for (let x = 0; x < grid[y].length; x++) {   
+            if (grid[y][x] < 4) {   
+                drawShip(x * cellSize, y * cellSize, grid[y][x] );}}}}
+
+
+function drawShip(x, y, s) {
+if (s == 0){ // if 0 then make a blank sea square
+    const sea = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    sea.setAttribute('x', x);
+    sea.setAttribute('y', y);
+    sea.setAttribute('width', cellSize);
+    sea.setAttribute('height', cellSize);
+    sea.setAttribute('fill', '#0080FF');
+    svg.appendChild(sea);}
+if (s == 1) { // if 1 then make a sea square with boat polygon on top
+    const sea = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    sea.setAttribute('x', x);
+    sea.setAttribute('y', y);
+    sea.setAttribute('width','100');
+    sea.setAttribute('height','100'); 
+    sea.setAttribute("fill", "#0080FF");
+    const boat = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    const pointsString = `${50 + x},${5 + y} ${75 + x},${30 + y} ${75 + x},${95 + y} ${25 + x},${95 + y} ${25 + x},${30+ y}`;
+    boat.setAttribute("points", pointsString);
+    boat.style.fill="#808080";
+    boat.style.stroke="black";
+    svg.appendChild(sea);
+    svg.appendChild(boat);}
+}
+drawGrid();
 console.log(grid);
 
 // Send the grid to the smart contract
