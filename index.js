23895ad2ed915efbd32612d2d9ca75d73c3d8d9f
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2023-03-29 15:00:48 +0200

    make svg boats

--- index.js
+++ index.js
@@ -22,25 +22,40 @@ for (let i = 1; i < 4; i++) {
     let y = prompt('Enter the y coordinate for your ship number ' + i);
     placeShip(x, y);
 }
-const cellSize = 50;
+const cellSize = 100;
 const svg = document.getElementById('gridcontainter');
 
 
 function drawGrid() {   
     for (let y = 0; y < grid.length; y++) {   
         for (let x = 0; x < grid[y].length; x++) {   
-            if (grid[y][x] === 1) {   
-                drawShip(x * cellSize, y * cellSize);}}}}
+            if (grid[y][x] < 4) {   
+                drawShip(x * cellSize, y * cellSize, grid[y][x] );}}}}
 
 
-function drawShip(x, y) {
-  const ship = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-  ship.setAttribute('x', x);
-  ship.setAttribute('y', y);
-  ship.setAttribute('width', cellSize);
-  ship.setAttribute('height', cellSize);
-  ship.setAttribute('fill', '#848482');
-  svg.appendChild(ship);
+function drawShip(x, y, s) {
+if (s == 0){ // if 0 then make a blank sea square
+    const sea = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    sea.setAttribute('x', x);
+    sea.setAttribute('y', y);
+    sea.setAttribute('width', cellSize);
+    sea.setAttribute('height', cellSize);
+    sea.setAttribute('fill', '#0080FF');
+    svg.appendChild(sea);}
+if (s == 1) { // if 1 then make a sea square with boat polygon on top
+    const sea = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    sea.setAttribute('x', x);
+    sea.setAttribute('y', y);
+    sea.setAttribute('width','100');
+    sea.setAttribute('height','100'); 
+    sea.setAttribute("fill", "#0080FF");
+    const boat = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    const pointsString = `${50 + x},${5 + y} ${75 + x},${30 + y} ${75 + x},${95 + y} ${25 + x},${95 + y} ${25 + x},${30+ y}`;
+    boat.setAttribute("points", pointsString);
+    boat.style.fill="#808080";
+    boat.style.stroke="black";
+    svg.appendChild(sea);
+    svg.appendChild(boat);}
 }
 drawGrid();
 console.log(grid);
@@ -55,4 +70,4 @@ const sendGridToContract = async () => {
   // Convert the grid to a string and send it to the contract
   const gridString = grid.toString();
   const tx = await contract.methods.setGameBoard(gridString).send({ from: playerAddress, gas: 100000 });
-}
+};
