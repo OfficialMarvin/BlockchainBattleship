f24ce80d8f96bcfca3d71274f4fe63a2fde585a9
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2023-04-12 20:48:03 +0200

    amazing comments

--- main.sol
+++ main.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Define the struct for each player's game state
+// struct for each players game state
 struct GameState {
     uint8[][] board;
     bool isTurn;
     address playerAddress;
 }
 
-// Define the contract
+// define contract
 contract Battleship {
     GameState public player1;
     GameState public player2;
 
     function setGameBoard(uint8[][] memory board) public {
 
-        // Check which player's turn it is and set their game state accordingly with the provided board
+        // check whose turn and set board and turns accordingly
         if (player1.isTurn) {
             require(player1.board.length == 0, "Player1's game board already set");
             player1.board = board;
@@ -28,7 +28,7 @@ contract Battleship {
             player1.isTurn = true;
         }
 
-        // Set the address of the caller as the current player's address
+        // make address of caller players addy
         if (player1.playerAddress == address(0)) {
             player1.playerAddress = msg.sender;
         } else {
@@ -36,12 +36,12 @@ contract Battleship {
         }
     }
 
-    // Function for a player to make a move
+    // for making moves
     function makeMove(uint8 x, uint8 y) public {
-        // Check that it is the player's turn
+        // check if players turn
         require((msg.sender == address(player1.playerAddress) && player1.isTurn) || (msg.sender == address(player2.playerAddress) && player2.isTurn), "Not your turn");
 
-        // Determine which player is making the move
+        // determine who is making move
         GameState storage gameState;
         if (msg.sender == address(player1.playerAddress)) {
             gameState = player2;
@@ -49,35 +49,35 @@ contract Battleship {
             gameState = player1;
         }
 
-        // Check that the move is within the bounds of the board
+        // check if move iin bound
         require(x < 5 && y < 5, "Move out of bounds");
 
-        // Check that the move has not already been made
+        // check if move made already
         require(gameState.board[x][y] < 2, "Move already made");
 
-        // Check if the move was a hit or a miss and update the board
+        // update array based on hit or miss
         if (gameState.board[x][y] == 1) {
             gameState.board[x][y] = 3; // Hit (3)
         } else {
             gameState.board[x][y] = 2; // Miss (2)
         }
         checkForWinner();
-        // Switch the turn to the other player
+        // switch turns
         player1.isTurn = !player1.isTurn;
         player2.isTurn = !player2.isTurn;
     }
 
-    // Getter function for player 1's game board
+    // get player 1s board
     function getPlayer1Board() public view returns (uint8[][] memory) {
         return player1.board;
     }
 
-    // Getter function for player 2's game board
+    // get player 2s board
     function getPlayer2Board() public view returns (uint8[][] memory) {
         return player2.board;
     }
     function checkForWinner() public {
-    // Check if player 1 has any remaining ships
+    // check if player 1 has ships
     bool player1HasShips = false;
     for (uint8 i = 0; i < 5; i++) {
         for (uint8 j = 0; j < 5; j++) {
@@ -91,16 +91,16 @@ contract Battleship {
         }
     }
     
-    // If player 1 has no more ships, declare player 2 the winner and reset the contract
+    // if player 1 has no ships, player 2 wins
     if (!player1HasShips) {
-        // Transfer the contract balance to player 2
+        // send contract balance to player 2
         payable(player2.playerAddress).transfer(address(this).balance);
-        // Reset the game
+        // reset game
         reset();
         return;
     }
 
-    // Check if player 2 has any remaining ships
+    // check if player 2 has ships
     bool player2HasShips = false;
     for (uint8 i = 0; i < 5; i++) {
         for (uint8 j = 0; j < 5; j++) {
@@ -114,20 +114,20 @@ contract Battleship {
         }
     }
     
-    // If player 2 has no more ships, declare player 1 the winner and reset the contract
+    // if player 2 has no ships, player 1 wins
     if (!player2HasShips) {
-        // Transfer the contract balance to player 1
+        // send contract balance to player 1
         payable(player1.playerAddress).transfer(address(this).balance);
-        // Reset the game
+        // reset
         reset();
         return;
     }
 }
 
-// Reset the game
-function reset() private {
-    player1.board = new uint8[][](5);
-    player2.board = new uint8[][](5);
+// reset function
+function reset() public {
+    player1.board = new uint8[][](0);
+    player2.board = new uint8[][](0);
     player1.isTurn = true;
     player2.isTurn = false;
     player1.playerAddress = address(0);
