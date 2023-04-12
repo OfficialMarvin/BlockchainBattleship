a7638842e46ba012f44a64428d09a1220d3dbb80
Marvin Jakobs <38141141+OfficialMarvin@users.noreply.github.com>
2023-04-12 17:10:17 +0200

    Pull enemy board

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Create a 5x5 grid
 const gridSize = 5;
-const grid = [];
+let grid = [];
 for (let i = 0; i < gridSize; i++) {
   grid[i] = new Array(gridSize).fill(0);
 }
@@ -63,58 +63,90 @@ console.log(grid);
 console.log(window.ethereum);
 const provider = new Web3.providers.HttpProvider('https://sepolia.infura.io/v3/ef929a0b34fa45c6b8758c57145b96b5');
 let web3;
+let egrid = [];
 
-async function fetchABI(web3) {
-  const response = await fetch('https://raw.githubusercontent.com/OfficialMarvin/BlockchainBattleship/main/abi.json');
-  const data = await response.json();
-  const myContract = new web3.eth.Contract(data, contractAddress);
-  console.log(myContract);
-  const coinbase = await web3.eth.getCoinbase();
-  const coinbaseString = coinbase.toString();
-  myContract.methods.setGameBoard(grid).send({ from: coinbaseString, gas: 1000000 })
-  .then((receipt) => {
-    console.log(grid);
-    console.log(receipt);
-  })
-  .catch((error) => {
-    // Handle error
-    console.error(error);
-  });
-  console.log("grid attempted send");
-}
 
 const contractAddress = '0x548eAA3DA0E4d8F305101a575d07AB2405f66A24';
+if (typeof window.ethereum !== 'undefined') {
+  try {
+    await window.ethereum.enable();
+    web3 = new Web3(window.ethereum);
+    console.log('Connected to Ethereum successfully!');
+    const userAddress = await web3.eth.getCoinbase();
+    console.log(userAddress);
+    const response = await fetch('https://raw.githubusercontent.com/OfficialMarvin/BlockchainBattleship/main/abi.json');
+    const data = await response.json();
+    const myContract = new web3.eth.Contract(data, contractAddress);
+    console.log(myContract); //Connected to blockchain
+    const coinbase = await web3.eth.getCoinbase();
+    const coinbaseString = coinbase.toString();
+    myContract.methods.setGameBoard(grid).send({ from: coinbaseString, gas: 1000000 })
+      .then((receipt) => {
+        console.log(grid);
+        console.log(receipt);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log("first grid sent"); //sent initial grid
+    let player1 = await myContract.methods.player1().call();
+    let player2 = await myContract.methods.player2().call();
+    let isPlayer1 = player1.playerAddress.toLowerCase() === coinbase.toLowerCase();
 
-async function sendGrid(web3) {
-  await fetchABI(web3);
-}
-
-async function connectEth(){
-  if (typeof window.ethereum !== 'undefined') {
-    try {
-      await window.ethereum.enable();
-      web3 = new Web3(window.ethereum);
-      console.log('Connected to Ethereum successfully!');
-      const userAddress = await web3.eth.getCoinbase();
-      console.log(userAddress);
-      sendGrid(web3);
-    } catch (error) {
-      console.error(error);
+    async function pullnUpdate() {
+      let egrid = [];
+      let player1Board = [];
+      let player2Board = [];
+      if (isPlayer1) {
+        while (player2Board.length == 0) {
+          player1Board = await myContract.methods.getPlayer1Board().call();
+          player2Board = await myContract.methods.getPlayer2Board().call();
+          await new Promise(resolve => setTimeout(resolve, 1000)); // wait for 1 second before checking again
+        }
+        grid = player1Board;
+        egrid = player2Board;
+        console.log("Player 2 board pulled");
+      } else {
+        while (player1Board.length == 0) {
+          player1Board = await myContract.methods.getPlayer1Board().call();
+          player2Board = await myContract.methods.getPlayer2Board().call();
+          await new Promise(resolve => setTimeout(resolve, 1000)); // wait for 1 second before checking again
+        }
+        grid = player2Board
+        egrid = player1Board;
+        console.log("Player 1 board pulled");
+      }
+    
+      // Update grids
+      drawGrid()
+      drawEGrid(egrid);
     }
-  } else {
-    console.log('Please install MetaMask to connect to the Ethereum network');
+    console.log(egrid)
+    if (isPlayer1 && player1.isTurn){
+      let x = prompt('Enter the x coordinate for your attack');
+      let y = prompt('Enter the y coordinate for your attack');
+      myContract.methods.makeMove(x,y).send({ from: coinbaseString, gas: 1000000 })
+    }
+    if (isPlayer1 == false && player2.isTurn){
+      let x = prompt('Enter the x coordinate for your attack');
+      let y = prompt('Enter the y coordinate for your attack');
+      myContract.methods.makeMove(x,y).send({ from: coinbaseString, gas: 1000000 })
+    }
+    pullnUpdate();
+  } catch (error) {
+    console.error(error);
   }
+} else {
+  console.log('Please install MetaMask to connect to the Ethereum network');
 }
 
-connectEth();
+
+  
+
 
 //Build enemy grid same way but do not show boats:
-const egrid = [];
-for (let i = 0; i < gridSize; i++) {
-  egrid[i] = new Array(gridSize).fill(0);
-}
 
-function drawEGrid() {   
+function drawEGrid(egrid) {   
   for (let y = 0; y < egrid.length; y++) {   
       for (let x = 0; x < egrid[y].length; x++) {   
           if (egrid[y][x] < 4) {   
@@ -122,7 +154,7 @@ function drawEGrid() {
 
 
 function drawEShip(x, y, s) {
-if (s < 2){ // if 0 then make a blank sea square
+if (s < 2){ // if 0 or 1 then make a blank sea square
   const sea = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
   sea.setAttribute('x', x);
   sea.setAttribute('y', y);
@@ -144,19 +176,24 @@ if (s == 3) { // if 3 then make a sea square with red boat polygon on top
   boat.style.stroke="black";
   enemysvg.appendChild(sea);
   enemysvg.appendChild(boat);}
-if (s == 2){
+if (s == 2){ //if 2 make yellow miss square
   const sea = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
   sea.setAttribute('x', x);
   sea.setAttribute('y', y);
   sea.setAttribute('width', cellSize);
   sea.setAttribute('height', cellSize);
-  sea.setAttribute('fill', '#ECFF00');
-  enemysvg.appendChild(sea);}
+  sea.setAttribute('fill', '#0080FF');
+  const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  circle.setAttribute('cx', x + cellSize/2);
+  circle.setAttribute('cy', y + cellSize/2);
+  circle.setAttribute('r', cellSize/4);
+  circle.setAttribute('fill', 'yellow');
+  
+  enemysvg.appendChild(sea);
+  enemysvg.appendChild(circle);}
 }
-drawEGrid();
 
 
-//PULL IN ENEMY GRID FROM CONTRACT THEN CREATE MAKEMOVE GAME LOOP DRAWING GRID AND EGRID AFTER EVERY TURN
 
 async function makeMove(playerIndex, x, y) {
   await myContract.methods.makeMove(x, y).send({ from: playerIndex });
